6cea965089e5e64c23f734c1300d0911c651be98
nguyenvancongdev <congthang.nuce@gmail.com>
2024-06-10 21:10:29 +0700

    g

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   build: {
     transpile: ['vuetify'],
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Home page</h1>
+    <h1>Home p  arrrge</h1>
     <!-- <NuxtLink>About page</NuxtLink> -->
   </div>
 </template>
