58f569e112990d3d0e6d3d61e31ca1e90a7ac9ce
nguyenvancongdev <congthang.nuce@gmail.com>
2024-06-24 21:44:01 +0700

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 cd dist
-echo vuejs-demo.congnguyen.dev > CNAME
+echo demo19-6-2024.cocode.pro > CNAME
 git init
 git add -A
 git commit -m "hello"
-git push -f https://github.com/nguyenvancongdev/vuejs-demo.git master:gh-pages
+git push -f https://github.com/nguyenvancongdev/demo19-6-2024.git main:gh-pages
 pause
 echo .
